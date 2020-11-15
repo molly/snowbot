21683f0c9ec76694201b97fc01ed83f0b7d7319b
Molly White <molly.white5@gmail.com>
2020-11-15 01:12:59 -0500

    Tis the season for the annual snowbot rewrite

---
+++ config.py
@@ -0,0 +1,13 @@
+# Be sure to update these values if you decide to run this bot!
+
+APP_NAME = "Boston Snowbot"
+REPO_URL = "https://github.com/molly/snowbot"
+
+OFFICE = "BYZ"
+GRID_X = 29
+GRID_Y = 64
+TIMEZONE = "US/Eastern"
+
+# OFFICE = "BOX"
+# GRID_X = 70
+# GRID_Y = 76
