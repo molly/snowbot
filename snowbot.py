a4333222d7557b954966a8d5110c169748ed29d0
Molly White <molly.white5@gmail.com>
2020-11-15 22:08:47 -0500

    Fix key check logic

--- snowbot.py
+++ snowbot.py
@@ -119,9 +119,7 @@ def diff_forecasts(current_forecast, prev_forecast, date_range):
     diff = {}
     for d in date_range:
         isodate = d.isoformat()
-        if (not prev_forecast or isodate not in prev_forecast) and current_forecast[
-            d
-        ] > 0:
+        if not prev_forecast or isodate not in prev_forecast or current_forecast[d] > 0:
             diff[d] = {"new": current_forecast[d]}
         elif prev_forecast and prev_forecast[isodate] != current_forecast[d]:
             diff[d] = {
